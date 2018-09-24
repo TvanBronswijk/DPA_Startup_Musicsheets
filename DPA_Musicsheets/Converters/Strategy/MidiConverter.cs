febe52cf4a7cfe696be0da12b02f99bb1ffb8983
Rick van Berlo <rickvanberlo@gmail.com>
2018-09-24 11:53:51 +0200

    midi converter update 1

--- DPA_Musicsheets/Converters/Strategy/MidiConverter.cs
+++ DPA_Musicsheets/Converters/Strategy/MidiConverter.cs
@@ -93,19 +93,18 @@ namespace DPA_Musicsheets.Converters.Strategy
                 switch (musicalSymbol.TokenKind)
                 {
                     case MusicToken.Kind.Note:
-                        var note = musicalSymbol;
 
                         // Calculate duration
-                        double absoluteLength = 1.0 / (double)note.Length;
-                        absoluteLength += (absoluteLength / 2.0) * note.Dots;
+                        double absoluteLength = 1.0 / (double)musicalSymbol.Length;
+                        absoluteLength += (absoluteLength / 2.0) * musicalSymbol.Dots;
 
                         double relationToQuartNote = _beatNote / 4.0;
                         double percentageOfBeatNote = (1.0 / _beatNote) / absoluteLength;
                         double deltaTicks = (sequence.Division / relationToQuartNote) / percentageOfBeatNote;
 
                         // Calculate height
-                        int noteHeight = notesOrderWithCrosses.IndexOf(note.Step.ToLower()) + ((note.Octave + 1) * 12);
-                        noteHeight += note.Alter;
+                        int noteHeight = notesOrderWithCrosses.IndexOf(musicalSymbol.Step.ToLower()) + ((musicalSymbol.Octave + 1) * 12);
+                        noteHeight += musicalSymbol.Alter;
                         notesTrack.Insert(absoluteTicks, new ChannelMessage(ChannelCommand.NoteOn, 1, noteHeight, 90)); // Data2 = volume
 
                         absoluteTicks += (int)deltaTicks;
