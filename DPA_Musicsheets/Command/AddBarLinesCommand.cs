dc0b46c508395d1eb3e252d276f5e5281c28184b
Rick van Berlo <rickvanberlo@gmail.com>
2018-10-25 10:21:13 +0200

    made barlines when pressing hotkey

--- DPA_Musicsheets/Command/AddBarLinesCommand.cs
+++ DPA_Musicsheets/Command/AddBarLinesCommand.cs
@@ -22,17 +22,24 @@ namespace DPA_Musicsheets.Command
             String previous = "";
             StringBuilder lilypondText = new StringBuilder();
             String lilypond = _textBox.Text;
-                foreach (string s in lilypond.Split(' ').Where(item => item.Length > 0))
+            foreach (string s in lilypond.Split(' ').Where(item => item.Length > 0))
             {
-                if (s == "\r\n")
+                if (s.Contains("\r\n"))
                 {
                     Match match = Regex.Match(previous, @"[a-z][0-9]");
-                    if (match.Success) lilypondText.Append("| ");
-                }
-
-                lilypondText.Append(s + " ");
-
+                    if (match.Success) lilypondText.Append("|");
+                    if(s == "\r\n")
+                    {
+                        lilypondText.Append(s + " ");
+                    }
+                    else
+                    {
+                        var tmp = s.Length;
+                        lilypondText.Append(" " + s.Substring(2) + " ");
+                    }
+                }else lilypondText.Append(s + " ");
                 previous = s;
+                
             }
             _textBox.Text = lilypondText.ToString();
         }
